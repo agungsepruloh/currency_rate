f077a1f85bf6fe0b52bd1273ae8c4e6e9cbd08d2
Agung Sepruloh <agungsepruloh1996@gmail.com>
2024-11-28 22:49:07 +0700

    [FEAT] currency.rate: Enable for version 18.0

--- account_currency_rate/__manifest__.py
+++ account_currency_rate/__manifest__.py
@@ -19,7 +19,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Accounting/Accounting',
-    'version': '17.0.0.0.1',
+    'version': '18.0.0.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'account'],
